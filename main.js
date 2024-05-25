3c1422c350f312b25d03a549f08ec9606dcf93a6
rabiyaka <illya.gerasimets@gmail.com>
2024-05-25 18:12:07 +0100

    added modal windows with info about projects

--- main.js
+++ main.js
@@ -1,118 +1,144 @@
-const themeToggle = document.getElementById('theme-toggle');
-const body = document.body;
+document.addEventListener("DOMContentLoaded", function () {
+    const themeToggle = document.getElementById('theme-toggle');
+    const body = document.body;
 
-themeToggle.addEventListener('change', () => {
-    body.classList.toggle('dark-theme');
-});
+    const profilePhoto = document.getElementById("profile-photo");
+    const modalPhoto = document.getElementById("modal-photo");
 
-const setTheme = (theme) => {
-    if (theme === 'dark') {
-        themeToggle.checked = true;
-        body.classList.add('dark-theme');
+    profilePhoto.addEventListener("click", function () {
+        modalPhoto.classList.add("active");
+    });
+
+    modalPhoto.addEventListener("click", function (event) {
+        modalPhoto.classList.remove("active");
+
+    });
+
+    themeToggle.addEventListener('change', () => {
+        body.classList.toggle('dark-theme');
+    });
+
+    const setTheme = (theme) => {
+        if (theme === 'dark') {
+            themeToggle.checked = true;
+            body.classList.add('dark-theme');
+        } else {
+            themeToggle.checked = false;
+            body.classList.remove('dark-theme');
+        }
+    };
+
+    const savedTheme = localStorage.getItem('theme');
+
+    if (savedTheme) {
+        if (savedTheme === 'dark') {
+            themeToggle.checked = true;
+            body.classList.add('dark-theme');
+        } else {
+            themeToggle.checked = false;
+            body.classList.remove('dark-theme');
+        }
     } else {
-        themeToggle.checked = false;
-        body.classList.remove('dark-theme');
+        const userPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+        setTheme(userPrefersDark ? 'dark' : 'light');
+
     }
-};
 
-const savedTheme = localStorage.getItem('theme');
+    // Save user preference on theme change
+    themeToggle.addEventListener('change', () => {
+        if (themeToggle.checked) {
+            localStorage.setItem('theme', 'dark');
+        } else {
+            localStorage.setItem('theme', 'light');
+        }
+    });
 
-if(savedTheme){
-    if (savedTheme === 'dark') {
-        themeToggle.checked = true;
-        body.classList.add('dark-theme');
-    } else {
-        themeToggle.checked = false;
-        body.classList.remove('dark-theme');
-    }
-} else{
-    const userPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-    setTheme(userPrefersDark ? 'dark' : 'light');
-    
-}
+    var modal = document.getElementById("modal");
+    var modalContent = document.querySelector(".modal-content");
+    var workExperienceList = document.getElementById("work-experience-list");
+    var modalBackground = document.getElementById('modal-background')
 
-// Save user preference on theme change
-themeToggle.addEventListener('change', () => {
-    if (themeToggle.checked) {
-        localStorage.setItem('theme', 'dark');
-    } else {
-        localStorage.setItem('theme', 'light');
-    }
-});
+    workExperienceList.addEventListener("click", function (e) {
+        if (e.target && e.target.nodeName == "LI") {
+            var id = e.target.getAttribute("data-id");
 
-var modal = document.getElementById("modal");
-var modalContent = document.querySelector(".modal-content");
-var workExperienceList = document.getElementById("work-experience-list");
+            let name;
+            let description;
+            let link;
 
+            projects_info.forEach(el => {
+                if (el.id == id) {
+                    name = el.name;
+                    description = el.description;
+                    link = el.link;
+                }
+            });
 
-workExperienceList.addEventListener("click", function (e) {
-    if (e.target && e.target.nodeName == "LI") {
-        var id = e.target.getAttribute("data-id");
+            var modalInfo = document.getElementById("modal-info");
 
-        let name;
-        let description;
-
-        projects_info.forEach(el => {
-            if (el.id == id) {
-                name = el.name;
-                description = el.description;
-            }
-        });
-
-        var modalInfo = document.getElementById("modal-info");
-        modalInfo.innerHTML = "";
-        modalInfo.classList.remove("typing");
-
-        // Устанавливаем новый текст с задержкой для корректного запуска анимации
-        setTimeout(() => {
             modalInfo.innerHTML = description;
-            modalInfo.classList.add("typing");
-        }, 100); // Немного подождем перед добавлением класса
 
-        document.getElementById("modal-title").innerHTML = name;
-        modal.classList.add("active");
-        modalContent.classList.add("active");
+            document.getElementById("modal-title").innerHTML = name;
+            var linkBlock = document.getElementById("link")
+            if (link) {
+                linkBlock.innerHTML = 'Link for a project: ' + link;
+            } else {
+                linkBlock.innerHTML = ''
+            }
+            modal.classList.add("active");
+            modalContent.classList.add("active");
+        }
+    });
+
+    // Закрытие модального окна при нажатии на крестик
+    var closeBtn = document.getElementsByClassName("close")[0];
+    closeBtn.onclick = function () {
+        modal.classList.remove("active");
+        modalContent.classList.remove("active");
     }
-});
 
-// Закрытие модального окна при нажатии на крестик
-var closeBtn = document.getElementsByClassName("close")[0];
-closeBtn.onclick = function () {
-    modal.classList.remove("active");
-    modalContent.classList.remove("active");
-}
+    modalBackground.onclick = function () {
+        modal.classList.remove("active");
+        modalContent.classList.remove("active");
+    }
 
 
+})
 
 let projects_info = [
     {
         id: 1,
         name: 'Save-Health (2022)',
         description: 'A site with an authorization and registration system, where each user has a personal account where you can add customer cards, each card is made according to a special form. In this form, you can also upload images on which you can put dots, and the program will calculate the angle between the dots.',
-        pictures: []
+        pictures: [],
+        link: ''
     },
     {
         id: 2,
         name: 'Amrita (July 2022 - March 2023)',
-        description: '<a href="https://amrita-shop.kiev.ua/">Amrita</a> (July 2022 - March 2023)',
-        pictures: []
+        description: 'Amrita - An online store for beauty and health products. I joined an existing project, worked on bug fixes and adding new features. Additionally, I migrated the project to the Laravel architecture.',
+        pictures: [],
+        link: '<a target="_blank" href="https://amrita-shop.kiev.ua/">https://amrita-shop.kiev.ua/</a>'
     },
     {
         id: 3,
         name: 'Cac-Tour/Cac-Edu (October 2023)',
-        description: '<a href="https://cac-tour.com">Cac-Tour</a>/<a href="https://cac-edu.com.ua">Cac-Edu</a>(October 2023)',
-        pictures: []
+        description: 'Cac-Tour/Cac-Edu - Two similar websites for selling tours and educational services for Ukrainian users. I joined an existing project and was responsible for bug fixes and website improvements for a month.',
+        pictures: [],
+        link: '<a target="_blank" href="https://cac-tour.com">https://cac-tour.com</a> and <a href="https://cac-edu.com.ua">https://cac-edu.com.ua</a>'
     },
     {
         id: 4,
         name: 'Estet.prom.ua (June 2023 - Now)',
-        description: '<a href="https://estet.prom.ua/ua/">Estet.prom.ua</a> (June 2023 - Now)',
-        pictures: []
+        description: ' Estet-Prom - An online store with a wide variety of products and categories.I served as a backend developer, responsible for parsing products from various websites and XML files, filtering the obtained products, and similar tasks. Specifically for the store, I developed a web site that runs on Flask (Python) and interacts with users via web sockets, featuring a separate admin panel.',
+        pictures: [],
+        link: '<a target="_blank" href="https://estet.prom.ua/ua/">https://estet.prom.ua/ua/</a>'
     },
     {
         id: 5,
         name: 'RedBird.cafe (November-December 2023)',
-        description: '<a href="https://www.redbird.cafe">RedBird.cafe</a> (November-December 2023)',
-        pictures: []
+        description: 'RedBird - A unique website for a restaurant chain where dishes are recommended to you by artificial intelligence based on your preferences and restrictions.I joined an existing project and was responsible for bug fixes and website improvements for two months.',
+        pictures: [],
+        link: '<a target="_blank" href="https://www.redbird.cafe">https://www.redbird.cafe</a>'
     },
 ]
